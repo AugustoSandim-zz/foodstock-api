74b81cab3a5627345d017f6f6a90fa9e31ced32e
Augusto Sandim <augusto@jera.com.br>
2015-12-21 22:59:33 -0300

    Create controller of foods

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
 	scope module: :api do
 		namespace :v1, defaults: { format: :json } do
+			
 			resources :categories, only: [:index, :create]
+			resources :categories, only: [:index, :create]
+
 		end
 	end
 
