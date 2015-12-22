21130c5d2c9f527e206e8b6805c40706d897b20e
Augusto Sandim <augusto@jera.com.br>
2015-12-21 23:48:59 -0300

    Create methods destroy show and update of foods

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -6,4 +6,46 @@ class Api::V1::FoodsController < Api::ApiController
 		render json: @foods, each_serializer: FoodSerializer
 	end
 
+	def create
+		@food = Food.create(food_params)
+
+		if @food.save
+			return render json: { message: "Food created" }, status: 201
+		else
+			return render json: {}, status: 422
+		end
+	end
+
+	def show
+		@food = Food.find_by(id: params[:id])
+
+		render json: @food, root: false, serializer: FoodSerializer
+	end
+
+	def update
+		@food = Food.find_by(id: params[:id])
+		
+		if @food.update_columns(food_params)
+			return render json: @food, serializer: FoodSerializer, root: false, status: 200
+		else
+			return render_invalid_params
+		end
+	end
+
+	def destroy
+		@food = Food.find_by(id: params[:id])
+
+		if @food.destroy
+			render json: {}, status: 204
+		else
+			render json: {}, status: 422 
+		end
+	end
+
+	private 
+		def food_params
+			params.fetch(:food, {}).permit(:name, :expiration_date, :brand, :quantity, :weight, 
+				:category)	
+		end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 		namespace :v1, defaults: { format: :json } do
 			
 			resources :categories, only: [:index, :create]
-			resources :foods, only: [:index, :create]
+			resources :foods, only: [:index, :create, :show, :destroy]
 
 		end
 	end
